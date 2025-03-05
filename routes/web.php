2b60f44ce1f7fe08dc20b865ed8dfeccae51e3b2
igaramadana <igrmdns085@gmail.com>
2025-03-05 14:42:21 +0700

    Jobsheet 2 - Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Basic Routing
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'Iga Ramadana Sahputra - 2341760083';
+});
+
+// Route Parameter
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' Komentar ke-:' . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+// Optional Parameter
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+// Route Name
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
